0bf7dab992b91d8583f9e7ecf10be30f9f407fbf
Yav Gz <gutierrez.alcantara.javier@gmail.com>
2023-06-26 16:10:15 -0600

    añadido de Js para elemnto del numero telefonico y CSS para animaciondes de las cards

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-alert("HI")
+const phonePicture = document.querySelector('.phone_picture');
+const tel = document.querySelector('.telefone')
+
+const toogle = () => {  
+  tel.classList.toggle('active')
+};
+
+phonePicture.addEventListener('click', toogle );
