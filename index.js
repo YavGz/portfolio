0b3a5aad1488887b5b10d249d99ef21f2418378b
Yav Gz <gutierrez.alcantara.javier@gmail.com>
2024-03-13 20:46:34 -0600

    add de TRC madrid

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const phonePicture = document.querySelector('.phone_picture');
 const tel = document.querySelector('.telefone')
+const cards = document.querySelectorAll('.card')
 
 const toogle = () => {  
   tel.classList.toggle('active')
