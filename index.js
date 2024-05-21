fac1e46e6e8a3c01acb6b1934be14c7f9f54c7da
Yav Gz <gutierrez.alcantara.javier@gmail.com>
2024-05-21 17:40:24 -0600

    Aviso de Cambio de version

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const phonePicture = document.querySelector('.phone_picture');
 const tel = document.querySelector('.telefone')
 const cards = document.querySelectorAll('.card')
+const close = document.querySelector('.close_modal')
+const modal = document.querySelector('.modal')
+
 
 const toogle = () => {  
   tel.classList.toggle('active')
 };
 
+const closeModal = () => {
+  modal.style.display = 'none'
+  // console.log('click en ' + closeModal);
+}
+
+close.addEventListener('click', closeModal);
 phonePicture.addEventListener('click', toogle );
+
+console.log(close);
