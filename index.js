2485e4cf5a9a99baf70c2c9507363241ca2d16b5
Yav Gz <gutierrez.alcantara.javier@gmail.com>
2024-05-23 23:43:18 -0600

    Header v2.0 terminado

--- index.js
+++ index.js
@@ -1,20 +1,63 @@
-const phonePicture = document.querySelector('.phone_picture');
-const tel = document.querySelector('.telefone')
-const cards = document.querySelectorAll('.card')
-const close = document.querySelector('.close_modal')
-const modal = document.querySelector('.modal')
+const headerLogo = document.querySelector('.header_logo');
+const headerLogoImg = document.querySelector('.header_logo__img');
+const headerHome = document.querySelector('.header_link__home');
+const headerProjects = document.querySelector('.header_link__projects');
+const headerAbout = document.querySelector('.header_link__about');
 
+const home = document.querySelector('.home_section')
+const projects = document.querySelector('.projects_section')
+const about = document.querySelector('.about_section')
 
-const toogle = () => {  
-  tel.classList.toggle('active')
+function switchSection (event) {
+  let elem = event.target.className;
+  
+
+  if (elem.includes('header_link__home')) {
+    console.log('click desde ' + elem);
+    
+
+    projects.classList.remove('section_active');
+    about.classList.remove('section_active');
+    home.classList.add('section_active');
+    headerHome.classList.add('header_link__active');
+    headerProjects.classList.remove('header_link__active');
+    headerAbout.classList.remove('header_link__active');
+    
+  } else if (elem.includes('header_logo')) {
+    console.log('click desde ' + elem);
+    
+    projects.classList.remove('section_active');
+    about.classList.remove('section_active');
+    home.classList.add('section_active');
+    headerHome.classList.add('header_link__active');
+    headerProjects.classList.remove('header_link__active');
+    headerAbout.classList.remove('header_link__active');
+
+  } else if (elem.includes('header_link__projects')) {
+    console.log('click desde ' + elem);
+    
+    projects.classList.add('section_active');
+    about.classList.remove('section_active');
+    home.classList.remove('section_active');
+    headerHome.classList.remove('header_link__active');
+    headerProjects.classList.add('header_link__active');
+    headerAbout.classList.remove('header_link__active');
+
+  } else if (elem.includes('header_link__about')) {
+    console.log('click desde ' + elem);
+    
+    projects.classList.remove('section_active');
+    about.classList.add('section_active');
+    home.classList.remove('section_active');
+    headerHome.classList.remove('header_link__active');
+    headerProjects.classList.remove('header_link__active');
+    headerAbout.classList.add('header_link__active');
+
+  }
 };
 
-const closeModal = () => {
-  modal.style.display = 'none'
-  // console.log('click en ' + closeModal);
-}
-
-close.addEventListener('click', closeModal);
-phonePicture.addEventListener('click', toogle );
-
-console.log(close);
+headerLogoImg.addEventListener('click', switchSection);
+headerLogo.addEventListener('click', switchSection);
+headerHome.addEventListener('click', switchSection);
+headerProjects.addEventListener('click', switchSection);
+headerAbout.addEventListener('click', switchSection);
